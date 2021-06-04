0f5491203cfe646a7883a1f94d59168b391a4742
Fred Lahde <frederick.lahde@worldiety.de>
2021-06-04 17:46:26 +0200

    Provide UserInput functions for specifc numeric types

--- snippets.go
+++ snippets.go
@@ -2,8 +2,8 @@ package snippets
 
 import (
 	"fmt"
-	"strconv"
 	"os"
+	"strconv"
 )
 
 func GetUserInputString(prompt string) string {
@@ -14,11 +14,51 @@ func GetUserInputString(prompt string) string {
 }
 
 func GetUserInputNumber(prompt string) int {
-    input := GetUserInputString(prompt)
-    i, err := strconv.Atoi(input)
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
-    return int(i)
+	input := GetUserInputString(prompt)
+	i, err := strconv.Atoi(input)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return int(i)
+}
+
+func GetUserInputNumberInt32(prompt string) int32 {
+	input := GetUserInputString(prompt)
+	i, err := strconv.ParseInt(input, 10, 64)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return int32(i)
+}
+
+func GetUserInputNumberInt64(prompt string) int64 {
+	input := GetUserInputString(prompt)
+	i, err := strconv.ParseInt(input, 10, 64)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return i
+}
+
+func GetUserInputNumberUint32(prompt string) uint32 {
+	input := GetUserInputString(prompt)
+	i, err := strconv.ParseUint(input, 10, 64)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return uint32(i)
+}
+
+func GetUserInputNumberUint64(prompt string) uint64 {
+	input := GetUserInputString(prompt)
+	i, err := strconv.ParseUint(input, 10, 64)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	return i
 }
